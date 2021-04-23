a903d5987e0748da94fd46fa9c97d64a9747551b
github username <github email address>
2021-04-23 17:22:45 -0400

    commit tree 08065748d57efb37421654983ee928ee2ebf817a

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+function mapToNegativize(arr){
+    return arr.map(function(ele) {
+        return ele === 0 ? ele : ele * -1
+    })
+}
+
+function mapToNoChange(arr) {
+    return arr.map(ele => ele)
+}
+
+function mapToDouble(arr) {
+    return arr.map(ele => ele * 2)
+}
+
+function mapToSquare(arr) {
+    return arr.map(ele => ele**2)
+}
+
+function reduceToTotal(arr, intVal=0) {
+    return arr.reduce(function(accm,ele){
+        return accm + ele
+    },intVal)
+}
+
+function reduceToAllTrue(arr) {
+    return !arr.map(ele => !!ele).includes(false)
+}
+
+function reduceToAnyTrue(arr) {
+    return arr.map(ele => !!ele).includes(true)
+}
